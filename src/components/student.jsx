572e0c74174a24b4f30e7bcd0a5b40e83816025c
Yossijaki <yossijaki@gmail.com>
2024-05-31 21:03:10 -0600

    style quickfix

--- src/components/student.jsx
+++ src/components/student.jsx
@@ -158,6 +158,7 @@ function Student() {
             >
               Edit
             </button>
+            &nbsp;
             <button
               type="button"
               className="btn btn-danger"
